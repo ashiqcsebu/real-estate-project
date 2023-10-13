adfdc349770f3ea9da7325cf82718f993c34bedb
Ashiqul Islam Shabu <ashiq.cse3.bu@gmail.com>
2023-10-13 21:48:25 +0600

    pages and router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import  { BrowserRouter,Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Profile from './pages/Profile'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  return <BrowserRouter>
+  <Routes>
+    <Route path='/' element ={<Home/>}></Route>
+    <Route path='/sign-in' element ={<SignIn/>}></Route>
+    <Route path='/sign-up' element ={<SignUp/>}></Route>
+    <Route path='/about' element ={<About/>}></Route>
+    <Route path='/profile' element ={<Profile/>}></Route>
+  </Routes>
 
-  return (
-    <>
-      <div>
-       
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  </BrowserRouter>
 }
-
-export default App
