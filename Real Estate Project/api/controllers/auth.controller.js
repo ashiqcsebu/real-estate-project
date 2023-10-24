774e7114020c3cfc48f59bbf78092325a5920c88
Ashiqul Islam Shabu <ashiq.cse3.bu@gmail.com>
2023-10-24 20:04:39 +0600

    Auth Done

---
+++ Real Estate Project/api/controllers/auth.controller.js
@@ -0,0 +1,38 @@
+import User from "../models/user.model.js";
+import becryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
+
+export const signUp = async (req, res, next) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = becryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashedPassword });
+  try {
+    await newUser.save();
+    res.status(201).json("User Created Successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User Not Found"));
+    }
+    const validPassword = becryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Wrong Credential"));
+    }
+    const token = jwt.sign({ _id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
--- Real Estate Project/api/index.js
+++ Real Estate Project/api/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
 
 dotenv.config();
 
-const { DB_USER, DB_PASS } = process.env;
+const { DB_USER, DB_PASS ,PORT } = process.env;
 
 mongoose
   .connect(
@@ -22,9 +25,20 @@ mongoose
   });
 
 const app = express();
-
-const PORT = process.env.PORT || 5000;
+app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use((err, req, res, next)=>{
+  const statusCode = err.status || 500 ;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success:false,
+    statusCode,
+    message
+  })
 });
