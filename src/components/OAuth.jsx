de53307b48ecc82f673b9e52b29834e9e297fbf7
Ashiqul Islam Shabu <ashiq.cse3.bu@gmail.com>
2023-10-26 02:39:41 +0600

    Google Sign Up Implemented  with advanced security

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,9 +1,34 @@
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import app from "../firebase";
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userSlice";
+import { useNavigate } from "react-router-dom";
+
 export default function OAuth() {
+  const dispatch = useDispatch();
+ const navigate = useNavigate()
   const handleGoogleClick = async () => {
     try {
-      console.log("a");
+      const auth = getAuth(app);
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+
+      const res = await fetch("/api/auth/google", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      dispatch(signInSuccess(data));
+      navigate('/')
     } catch (error) {
-      console.log("Could Not Sign In with Google", error);
+      console.error("Could Not Sign In with Google", error);
     }
   };
   return (
